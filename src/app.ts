3dc624f07ad6ea77517d758aeb901104a714e821
flamrdevs <flamrdevs@gmail.com>
2023-12-12 09:52:35 +0700

    chore: env

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,18 @@ import { HTTPException } from "hono/http-exception";
 import { compress } from "hono/compress";
 import { cors } from "hono/cors";
 
+const env = {
+  APP_NAME: process.env.APP_NAME,
+};
+
 const app = new Hono();
 
 app.use("*", cors({ origin: "*" }), compress());
 
+app.get("/env", (ctx) => ctx.json(env));
+
 app
-  .get("/", (ctx) => ctx.json({ name: process.env.APP_NAME }, 200))
+  .get("/", (ctx) => ctx.json({ name: env.APP_NAME }, 200))
   .notFound((ctx) => ctx.json({ message: "Not found" }, 404))
   .onError((err, ctx) => {
     if (err instanceof HTTPException) return err.getResponse();
